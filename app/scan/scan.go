b8219cb641ba145a2522aec1165e081693c01785
Eric Timmerman <eet1992@gmail.com>
2020-11-11 19:49:42 -0800

    Initial scan function
    This builds out the initial scan function. Currently takes first arg as
    IP and second as port. Will test then build out options for sending args
    by name.

--- app/scan/scan.go
+++ app/scan/scan.go
@@ -1,5 +1,32 @@
 package scan
 
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
 func Run(args []string) error {
+	db, err := sql.Open("mysql", buildConnString(args[0], args[1]))
+	if err != nil {
+		errors.Wrapf(err, "Failed to open connection, no MySQL at IP: %s with Port %s", args[0], args[1])
+	}
+
+	defer db.Close()
+
+	var version string
+
+	err = db.QueryRow("SELECT VERSION()").Scan(&version)
+	if err != nil {
+		errors.Wrap(err, "Failed to get MySQL version")
+	}
+
+	fmt.Printf("Connected to MySQL server at IP: %s with Port: %s\nReceived version: %s from database", args[0], args[1], version)
+
 	return nil
+}
+
+func buildConnString(IP, port string) string {
+	return fmt.Sprintf("tcp(%s:%s)", IP, port)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/clantant/mys_sweepsy
 go 1.14
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
 )
